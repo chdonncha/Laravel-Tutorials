ac9fe830ebec7672d15d8d2cdae3e51cdbe5c2c1
chdonncha <chdonncha@gmail.com>
2016-08-23 17:17:52 +0100

    finished Eloquent ORM Tutorial

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,19 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
-use App\Http\Requests;
+
+use App\TodoList;
+use App\User;
 
 class TodoListController extends Controller
 {
     public function index()
     {
-    	return view('todos.index');
+    	$todo_lists = TodoList::all();
+    	return view('todos.index')->with('todo_lists', $todo_lists);
     }
 
     public function show($id)
     {
-    	return view('todos.show')->withId($id);
+    	$list = TodoList::find($id);
+    	return view('todos.show')->withList($list);
     }
+
+    public function create()
+    {
+    	$list = new TodoList();
+    	$list->name = "Another List";
+    	$list->save();
+    	return "Create a new List";
+    }
+
+   	public function store()
+   	{
+
+   	}
 }
