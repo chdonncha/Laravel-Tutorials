06e6437db476df42bf1246c9c7aa3fc085e2bfea
chdonncha <chdonncha@gmail.com>
2016-08-24 11:29:11 +0100

    Fixed csrf_token problem

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 	<form method="POST" action="http://localhost/laravel/laravel-basics/public/todos" accept-charset="UTF-8">
 	<meta name="csrf-token" content="{{ csrf_token() }}" >
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		<label for="title">List Title</label>
 		<input name="title" type="text" id="title"></label>
 		<input class="button" type="submit" value="submit"></label>
