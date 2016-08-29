d38996a618c79a31e2e12373e436f3c0ddf635bb
chdonncha <chdonncha@gmail.com>
2016-08-29 13:38:40 +0100

    update, cannot find TodoList

--- app/Http/Controllers/TodoItemController.php
+++ app/Http/Controllers/TodoItemController.php
@@ -14,9 +14,29 @@ class TodoItemController extends Controller
     	return View::make('items.create')->withTodoList($todo_list);
     }
 
-      public function store()
+      public function store($list_id)
     {
-    	
+    	$todo_list = TodoList::findOrFail($list_id);
+
+
+   		// define rules
+   		$rules = array(
+   				'content' => array('required')
+   			);
+
+   		// pass input to validator
+   		$validator = Validator::make(Input::all(), $rules);
+
+   		// test if input is valid
+   		if ($validator->fails()) {
+   			return Redirect::route('todos.items.create', $list_id)->withErrors($validator)->withInput();
+   		}
+
+   		$item = new TodoItem();
+    	$item->content = Input::get('content');
+    	$todo_list->listItems()->save($item);
+    	return Redirect::route('todos.show', $todo_list->id)->withMessage('Item Was Added!');
+
     }
 
       public function edit($id)
@@ -29,8 +49,4 @@ class TodoItemController extends Controller
     	
     }
 
-         public function update($id)
-    {
-    	
-    }
 }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -5,6 +5,11 @@
 		@foreach ($items as $item)
 			<h4>{{{ $item->content }}}</h4>
 		@endforeach
-		<p>{{ link_to_route('todos.index', 'back')}}
+		<ul class="no-bullet button-group">
+			<li>
+				{{ link_to_route('todos.items.create', 'add new item', [$list->id], ['class' => 'button success'])}}
+			</li>
+		</ul>
+		<p>{{ link_to_route('todos.index', 'back', null, ['class' => 'button'])}}</p>
 	</div>
 @stop
